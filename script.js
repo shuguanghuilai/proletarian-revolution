bd7d707badc695462e2424d1992b35d5160df8a3
shuguanghuilai <158572368+shuguanghuilai@users.noreply.github.com>
2024-02-03 22:42:47 +0800

    修改 法律条文-法律描述

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ const options = {
       countryRegulations: [
         {
           label: '中华人民共和国劳动法',
-          desc: '简称《劳动法》，是一部中华人民共和国法律。于1995年1月1日生效。',
+          desc:
+            `
+              第三十八条&nbsp;&nbsp;&nbsp;&nbsp;如果公司没有及时支付足额工资，劳动者可以走被迫离职流程。
+              <br />
+              第四十条&nbsp;&nbsp;&nbsp;&nbsp;提前三十天告知辞退或支付一个月的全额工资（通知金）。
+              <br />
+              第四十七条&nbsp;&nbsp;&nbsp;&nbsp;公司工龄 N 年，赔 N 个月（补偿金）。
+            `,
           url: 'https://www.gov.cn/flfg/2007-06/29/content_669394.htm'
         },
         {
           label: '工资支付暂行规定',
-          desc: '是为维护劳动者通过劳动获得劳动报酬的权利，规范用人单位的工资支付行为，根据《中华人民共和国劳动法》有关规定制定。由劳动部于1994年12月6日发布，自1995年1月1日起执行。',
+          desc:
+            `
+              第十二条&nbsp;&nbsp;&nbsp;&nbsp;劳动者被迫停工且非本人造成的，停工首月需要支付全额工资。
+            `,
           url: 'https://www.gov.cn/zhengce/2022-08/31/content_5711284.htm'
         },
         {
@@ -25,7 +35,10 @@ const options = {
       localRegulations: [
         {
           label: '深圳市员工工资支付条例',
-          desc: '根据《中华人民共和国劳动法》等法律、法规，结合深圳市实际，制定《深圳市员工工资支付条例》。2004年8月27日深圳市第三届人民代表大会常务委员会第三十三次会议通过，2004年9月24日广东省第十届人民代表大会常务委员会第十三次会议批准，并经历2009年、2019年、2022年三次修订。该条例共十章节六十二条。',
+          desc:
+            `
+              第二十八条&nbsp;&nbsp;&nbsp;&nbsp;劳动者被迫停工且非本人造成的，停工首月需要支付全额工资。
+            `,
           url: 'https://www.gd.gov.cn/zwgk/wjk/zcfgk/content/post_2532037.html'
         },
         {
@@ -37,6 +50,16 @@ const options = {
           label: '上海市企业工资支付办法',
           desc: '在二O一六年六月二十七日 向 各有关委、办、局，各控股（集团）公司、企业（集团）公司，各区、县劳动和社会保障局 印发了的《上海市企业工资支付办法》，其目的是为维护劳动者通过劳动获得劳动报酬的权利，规范企业的工资支付行为。',
           url: 'https://ghpf.acftu.org/flfg/dfldfg/sjzbsh/202009/t20200929_547180.html?7OkeOa4k=qAc4qqk45dB45dB45aHe.UbzeAoDBlpbg0vbAEnoGQ0qqqqqqqqqqqqq5a'
+        },
+        {
+          label: '北京市工资支付规定',
+          desc: '是北京市人民政府于2003年12月22日颁布，为规范工资支付行为和维护劳动者的合法权益，根据《中华人民共和国劳动法》及有关法律、法规的规定而制定。《规定》共四十五条，自2004年1月22日起施行。1994年11月16日市政府第25号令发布、1997年12月31日市政府第12号令修改的《规定》第九条同时废止。',
+          url: 'https://www.beijing.gov.cn/zhengce/zhengcefagui/201905/t20190522_56550.html'
+        },
+        {
+          label: '浙江省企业工资支付管理办法',
+          desc: '已经省人民政府第81次常务会议审议通过，现予公布，自2017年5月1日起施行。本办法共五章三十八条，自2017 年5 月1 日起施行。',
+          url: 'https://www.zj.gov.cn/zjservice/item/detail/lawtext.do?outLawId=99c1ab66-5d2d-4199-aec6-cbf66b3d0c9c&impleType=11'
         }
       ],
 
